06e679ac89613174712bb3e246fc122c06f812cf
delet256 <44375612+delet256@users.noreply.github.com>
2018-10-25 21:37:28 +0300

    Add files via upload

---
+++ TestWebApi/TestWebApi/Controllers/MessageController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestWebApi.Models;
+
+namespace WebApplication2.Controllers
+{
+
+
+
+
+    [Route("api/Message")]
+    public class ValuesController : Controller
+    {
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]RequestParameters item)
+        {
+            var parameters = new NameValueCollection {
+                { "token", item.TokenApi },
+                { "user", item.UserKey },
+                { "message", item.Message }
+            };
+
+            using (var client = new WebClient())
+            {
+                client.UploadValues("https://api.pushover.net/1/messages.json", parameters);
+            }
+
+        }
+
+
+    }
+}
---
+++ TestWebApi/TestWebApi/Models/RequestParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApi.Models
+{
+    public class RequestParameters
+    {
+        public string TokenApi { get; set; }
+        public string UserKey { get; set; }
+        public string Message { get; set; }
+
+    }
+}
